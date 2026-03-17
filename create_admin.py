f16a0cdc711a861cd377a64abfa72ba46e90b8d2
Shrivatsan <shrivatsan28@gmail.com>
2026-03-17 23:11:30 +0530

    Auto-create admin user during deployment

---
+++ create_admin.py
@@ -0,0 +1,7 @@
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+    print("Superuser 'admin' with password 'admin' created successfully.")
+else:
+    print("Superuser 'admin' already exists.")
